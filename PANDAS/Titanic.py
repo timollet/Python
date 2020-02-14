48441439456805e4117c711410cb3c18467e50f4
GOIBEAULT <timothee-swann.goibeault@sogeti.com>
2020-02-14 16:19:00 +0100

    Ajout de l'exercice sur Titanic

--- PANDAS/Titanic.py
+++ PANDAS/Titanic.py
@@ -30,5 +30,31 @@ plt.figure()
 data['pclass'].value_counts().plot.bar()
 plt.figure()
 data['age'].hist()
-# groupby comme en sql
+# Stats moyennes selon la classe et le sexe
+print('--------------------------------------')
+print ('\n Stats selon la classe et le sexe \n')
 print(data.groupby(['sex','pclass']).mean())
+print('\n')
+# le nombre de mineur par classe
+print('--------------------------------------')
+print ('\n Nombre de mineur selon la classe \n')
+print(data[data['age'] < 18]['pclass'].value_counts())
+
+# Stats des moyennes sur les mineur selon leur sex et classe
+print('--------------------------------------')
+print ('\n Stats des mineurs selon la classe et le sexe \n')
+print(data[data['age'] < 18].groupby(['sex','pclass']).mean())
+
+# Indexing comme numpy avec iloc (index localisation)
+print('--------------------------------------')
+print(data.iloc[0:2,0:2])
+print('--------------------------------------')
+print(data.loc[0:2,['age','sex']])
+
+#Excercice Feature Engineering (categorie d'age)
+print('--------------------------------------')
+data[ data.loc[:,'age']<21 ] = 0
+data[ (data.loc[:,'age']>20) & (data.loc[:,'age']<31)] = 1
+data[ (data.loc[:,'age']>30) & (data.loc[:,'age']<41)] = 2
+data[ data.loc[:,'age']>40 ] = 3
+print(data['age'].value_counts())
